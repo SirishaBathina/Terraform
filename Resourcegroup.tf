8f6d7cad613d8ff4212279fff4f213581836f3ac
SirishaBathina <141664760+SirishaBathina@users.noreply.github.com>
2023-08-10 10:04:16 +0100

    Create Resourcegroup.tf

---
+++ Resourcegroup.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+provider "azurerm" {
+
+  subscription_id="7e5dce23-8be8-4881-8214-c8fd9b80ee51"
+  tenant_id = "afdeecf7-3eaf-4fe8-8066-aa259eaf0aec"
+  client_id = "cfa961ea-d784-42f8-a8ce-965fafc0c330"
+  client_secret = "2.H8Q~yoOmiGVg5eGwj~JH-BNql~t8mYIBz8Rb~G"
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grp"
+  location = "North Europe"
+}
