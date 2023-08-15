1d2a3c8600666593e8a6d378250989ebba811097
SirishaBathina <141664760+SirishaBathina@users.noreply.github.com>
2023-08-15 15:16:42 +0100

    Update Statefile.tf

--- Statefile.tf
+++ Statefile.tf
@@ -8,15 +8,15 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "urv-remote-state-rg"
-    storage_account_name  = "urvstorageaccount"
+    resource_group_name   = "ap-remote-state-rg"
+    storage_account_name  = "apstorageaccount"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "urv-resource-group"
+  name     = "siri-resource-group"
   location = "East US"
 }
 
